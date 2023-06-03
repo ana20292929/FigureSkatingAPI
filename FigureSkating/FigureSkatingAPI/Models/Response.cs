a55cb433b6cb351e5c460958ae18cdefac0665f5
ana20292929 <ana.lomidze29@gmail.com>
2023-06-03 14:24:19 -0400

    fixed API

---
+++ FigureSkating/FigureSkatingAPI/Models/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FigureSkatingAPI.Models
+{
+	public class Response
+	{
+		public int statusCode { get; set; }
+		public string statusDescription { get; set; }
+		public List<Skater> SkatersList { get; set; } = new(); //list for Skaters
+		public List<Competition> CompetitionsList { get; set; } = new(); //list for Competitions
+	}
+}
+
